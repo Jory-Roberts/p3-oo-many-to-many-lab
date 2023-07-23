aa8aa77d5b7a311486cb8e57ee95252f85edde5a
Jory Roberts <robertsjory13@gmail.com>
2023-07-23 14:15:18 -0500

    properties and setters

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,83 @@
 class Author:
-    pass
+    all = []
+
+    def __init__(self, name):
+        self.name = name
+
+
 
 
 class Book:
-    pass
+    all = []
+
+    def __init__(self, title):
+        self.title = title
+        Book.all.append(self)
 
 
 class Contract:
-    pass
+    all = []
+
+    def __init__(self, author, book, date, royalties):
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+        Contract.all.append(self)
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, value):
+        if not isinstance(value, Author):
+            raise TypeError('Author must be an instance of the Author Class.')
+        self._author = value
+
+    @property
+    def book(self):
+        return self._book
+
+    @book.setter
+    def book(self, value):
+        if not isinstance(value, Book):
+            raise TypeError('Book must be an instance of the Book Class.')
+        self._book = value
+
+    @property
+    def date(self):
+        return self._date
+
+    @date.setter
+    def date(self, value):
+        if not isinstance(value, str):
+            raise TypeError('Date must be of type string.')
+        self._date = value
+
+    @property
+    def royalties(self):
+        return self._royalties
+
+    @royalties.setter
+    def royalties(self, value):
+        if not isinstance(value, int):
+            raise TypeError('Royalties must be of type int.')
+        self._royalties = value
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
